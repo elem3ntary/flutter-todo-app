3b22d7a1be34b627d497d4421e995f817d4772b9
Stefan <impr0digg@gmail.com>
2023-01-27 09:27:08 +0200

    Implement task list

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ Add a new task | I can capture work to be done | New task is created
 Mark task as completed | I can keep track of completed tasks | Selected task is mark as completed and is remove from task list
 Tasks to be persistant | I can access them any time | Tasks are stored in storage
 
+[Color pallete used](https://coolors.co/palette/0d1b2a-1b263b-415a77-778da9-e0e1dd)
+
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TaskState(),
+      child: const MainApp(),
+    ),
+  );
+}
+
+class Task {
+  bool done = false;
+  String name;
+  Task(this.name);
+}
+
+class TaskState extends ChangeNotifier {
+  var tasks = <Task>[];
+
+  void addTask(Task task) {
+    tasks.add(task);
+    notifyListeners();
+  }
+
+  void markTaskAsCompleted(Task task) {
+    int selectedTask = tasks.indexOf(task);
+    tasks[selectedTask].done = true;
+    notifyListeners();
+  }
+
+  List<Task> getTasks() {
+    return tasks.where((element) => !element.done).toList();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -9,11 +41,96 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var state = context.watch<TaskState>();
+    var defaultColorScheme = ThemeData.dark().colorScheme;
+    var baseTheme = ThemeData.dark();
+    var themeData = baseTheme.copyWith(
+      textTheme: GoogleFonts.montserratTextTheme(baseTheme.textTheme),
+      colorScheme: defaultColorScheme.copyWith(
+          background: const Color(0xff1B263B),
+          secondary: const Color(0xff778DA9)),
+    );
+    return MaterialApp(
+      theme: themeData,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: themeData.colorScheme.background,
+        body: const MainPage(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => state.addTask(Task('test')),
+          child: const IconTheme(
+            data: IconThemeData(color: Colors.white),
+            child: Icon(Icons.add),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var state = context.watch<TaskState>();
+    var tasks = state.getTasks();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: ((context, index) => TodoTile(tasks[index]))),
+        ),
+      ],
+    );
+  }
+}
+
+class TodoTile extends StatelessWidget {
+  const TodoTile(
+    this.task, {
+    super.key,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    var state = context.watch<TaskState>();
+    var iconData = task.done ? Icons.circle : Icons.circle_outlined;
+    return Container(
+      height: 72,
+      margin: const EdgeInsets.symmetric(vertical: 9, horizontal: 30),
+      decoration: BoxDecoration(
+          border: Border.all(width: 1, color: Colors.white),
+          borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 22),
+            child: IconButton(
+              onPressed: () {
+                state.markTaskAsCompleted(task);
+              },
+              icon: Icon(
+                iconData,
+                size: 35,
+                weight: 1,
+              ),
+            ),
+          ),
+          Text(
+            'some title',
+            style: TextStyle(
+              fontSize: Theme.of(context).textTheme.headlineSmall!.fontSize,
+            ),
+          ),
+        ],
       ),
     );
   }
