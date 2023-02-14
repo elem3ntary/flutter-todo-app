639fa8c198d34eade4ff6d956c006089668a3180
Stefan <impr0digg@gmail.com>
2023-02-14 15:08:29 +0200

    Optimize imports

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,7 +1,5 @@
-import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/models/task.dart';
 import 'package:todo_app/persistance.dart';
 
 class Task {
